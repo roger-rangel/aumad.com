b69ec9271cc168ebca15a07a8226b5cd761e3fed
Hasan Bhati <hasans.bhati@gmail.com>
2022-02-17 14:35:01 +0100

    comingsoon1.6

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+    var CountDownDate = new Date("Jan 1, 2023 00:00:00").getTime();
+    
+    var x = setInterval(function(){
+        var now = new Date().getTime();
+        var distance = CountDownDate * now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(distance % (1000 * 60 * 60 * 24) / ( 1000 * 60 * 60));
+    var minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+    var seconds = Math.floor(distance / (100 * 60) / 1000);
+
+    document.getElementByClass("launch").innerHTML = days + "d" + hours + "h" + minutes + "m" + seconds + "s";
+
+    if (distance < 0){
+        clearInterval(x);
+        document.getElementsByClass("Shortly").innerHTML = "EXPIRED";
+    }
+    }, 1000)
+    
